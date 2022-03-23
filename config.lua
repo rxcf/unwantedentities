abd7912d6a5b0429a38bda765c8f3e8ba3656e0d
Rey <80929479+RE3RY@users.noreply.github.com>
2022-03-23 15:42:21 +0200

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,38 @@
+Config = {}
+
+Config.BlacklistedScenarios = {
+    ['TYPES'] = {
+        "WORLD_VEHICLE_MILITARY_PLANES_SMALL",
+        "WORLD_VEHICLE_MILITARY_PLANES_BIG",
+    },
+    ['GROUPS'] = {
+        2017590552,
+        2141866469,
+        1409640232,
+        `ng_planes`,
+    }
+}
+
+Config.BlacklistedVehicles = {
+    [`police`] = true,     -- Police Stanier
+    [`police2`] = true,    -- Police Buffalo
+    [`police3`] = true,    -- Police Interceptor
+    [`police4`] = true,    -- Undercover Police Stanier
+    [`policeb`] = true,    -- Police Bike
+    [`policet`] = true,    -- Police Transport Van
+    [`policeold1`] = true, -- Snowy Police Rancher
+    [`policeold2`] = true, -- Snowy Police Esperanto
+    [`POLMAV`] = true,     -- Police Helicopter
+    [`ambulance`] = true,  -- Ambulance
+    [`fbi`] = true,        -- FIB Buffalo
+    [`fbi2`] = true,       -- FIB Granger
+}
+
+Config.BlacklistedPeds = {
+    [`s_m_y_ranger_01`] = true,
+    [`s_m_y_sheriff_01`] = true,
+    [`s_m_y_cop_01`] = true,
+    [`s_f_y_sheriff_01`] = true,
+    [`s_f_y_cop_01`] = true,
+    [`s_m_y_hwaycop_01`] = true,
+}
