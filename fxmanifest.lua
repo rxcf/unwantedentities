62607b22af169efad5f4f6015a94ed4b7db2bf9d
rxcf <80929479+rxcf@users.noreply.github.com>
2023-08-20 14:43:03 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,9 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'RE3RY#6918'
-description 'https://github.com/RE3RY/unwantedentities'
+name 'unwantedentities'
 version '1.0.0'
+description 'Small resource to remove any kind of unwanted entities (cars, npcs & scenarios)'
+author 'rxcf'
 
 shared_script 'config.lua'
 server_script 'server/*.lua'
